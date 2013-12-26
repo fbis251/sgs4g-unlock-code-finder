bad4702ddb8d1f01bc7ca7d777033aeb63312cb3
Fernando Barillas <fbis251@gmail.com>
2013-12-26 10:08:40 -0800

    Add unlocker header file
    Signed-off-by: Fernando Barillas <fbis251@gmail.com>
    
    Signed-off-by: Fernando Barillas <fbis251@gmail.com>

--- unlocker.c
+++ unlocker.c
@@ -11,41 +11,7 @@
  * Compilation Instructions: To compile with the codesourcery cross compiler,
  *     arm-none-linux-gnueabi-gcc --static -o unlock_code unlocker.c
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifndef TRUE
-#define TRUE 1
-#endif
-
-#ifndef FALSE
-#define FALSE 0
-#endif
-
-#ifndef NV_DATA_PATH
-#define NV_DATA_PATH "/efs/root/afs/settings/nv_data.bin"
-#endif
-
-#ifndef CODE_LOCATION
-#define CODE_LOCATION 0x146E // Magic byte location for ASCII code
-#endif
-
-#ifndef CODE_LENGTH
-#define CODE_LENGTH 8 // How many characters is the unlock code?
-#endif
-
-#ifndef ASCII_0
-#define ASCII_0 '0'
-#endif
-
-#ifndef ASCII_9
-#define ASCII_9 '9'
-#endif
-
-#ifndef EXIT_FAILURE
-#define EXIT_FAILURE 1
-#endif
+#include "unlocker.h"
 
 static const int DEBUG = FALSE;
 static char ** programPath;
---
+++ unlocker.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef NV_DATA_PATH
+#define NV_DATA_PATH "/efs/root/afs/settings/nv_data.bin"
+#endif
+
+#ifndef CODE_LOCATION
+#define CODE_LOCATION 0x1469 // Magic byte location for lock status + ASCII code
+#endif
+
+#ifndef CODE_LENGTH
+#define CODE_LENGTH 13 // How many bytes are the lock status + ASCII code?
+#endif
+
+#ifndef CODE_OFFSET
+#define CODE_OFFSET 5 // ASCII unlock code compared to CODE_LOCATION
+#endif
+
+#ifndef ASCII_0
+#define ASCII_0 '0'
+#endif
+
+#ifndef ASCII_9
+#define ASCII_9 '9'
+#endif
+
+#ifndef UNLOCKED
+#define UNLOCKED 0
+#endif
+
+#ifndef LOCKED
+#define LOCKED 1
+#endif
+
+#ifndef EXIT_FAILURE
+#define EXIT_FAILURE 1
+#endif
+
+/* Function declarations */
+void printUsage();
+void readUnlockCode( const char * );
+
+
